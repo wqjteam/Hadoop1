c3f876249d19261afb606b811a2ec961dd823dde
githubwqj <893453172@qq.com>
2018-03-20 18:41:50 +0800

    1

--- src/main/java/com/wqj/mapreduces/WordCountDriver.java
+++ src/main/java/com/wqj/mapreduces/WordCountDriver.java
@@ -13,37 +13,43 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 /**
  * @Description: 启动计算程序,需要再次封装我们的MapReduce程序相关的参数,指定jar包,提交给yarn
  * @author wqj
- * @date 2018年3月20日 下午2:13:48 
+ * @date 2018年3月20日 下午2:13:48
  */
 public class WordCountDriver {
 
 	public static void main(String[] args) throws Exception {
 		Configuration configuration = new Configuration();
-		
-		
+
+		configuration.set("","");
 		Job job = Job.getInstance(configuration);
-		
-//		job.setJar(jar);
-		//指定本程序所在jar的所在路进
+
+		// job.setJar(jar);
+		// 指定本程序所在jar的所在路进
 		job.setJarByClass(WordCountDriver.class);
-		
-		//指定本业务的job要是用的map和maprducer类
+
+		// 指定本业务的job要是用的map和maprducer类
 		job.setMapperClass(MapDemo.class);
 		job.setReducerClass(ReducesDemo.class);
-		
-		//指定mapper输出类型
+
+		// 指定mapper输出类型
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
-		//指定最终输出数据的KV类型
+
+		// 指定最终输出数据的KV类型
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
-		//指定job的输入原始目录
+
+		// 指定job的输入原始目录
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
-		
-		//指定job的输出结果
+
+		// 指定job的输出结果
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
+		// 将job 中配置相关参数,以及job所有java类所在的jar包,提交给yarn运行
+		// job.submit();
+
+		boolean waitForCompletion = job.waitForCompletion(true);
+		//判断是否成功
+		System.exit(waitForCompletion?0:1);
 	}
 }
