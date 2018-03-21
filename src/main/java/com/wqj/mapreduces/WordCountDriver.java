1b613920bf0f88ee6a59b27ef3ff95f69b4766bb
githubwqj <893453172@qq.com>
2018-03-21 15:52:27 +0800

    5

--- src/main/java/com/wqj/mapreduces/WordCountDriver.java
+++ src/main/java/com/wqj/mapreduces/WordCountDriver.java
@@ -1,7 +1,5 @@
 package com.wqj.mapreduces;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
