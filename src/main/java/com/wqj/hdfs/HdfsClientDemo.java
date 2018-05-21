4132f36ac7cb5ee36f1a08efcce66089cbfe1d74
githubwqj <893453172@qq.com>
2018-05-21 11:05:52 +0800

    1

--- src/main/java/com/wqj/hdfs/HdfsClientDemo.java
+++ src/main/java/com/wqj/hdfs/HdfsClientDemo.java
@@ -1,51 +1,51 @@
-package com.wqj.hdfs;
-
-import java.io.InputStream;
-import java.net.URI;
-import java.util.Properties;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.junit.Before;
-import org.junit.Test;
-
-
-
-
-/**
- * @Description: TODO
- * @author wqj
- * @date 2018年3月18日 下午8:36:53 
- */
-public class HdfsClientDemo {
-	
-	
-	FileSystem fileSystem =null;
-	@Before
-	public void init() throws Exception {
-		Properties properties= new Properties();
-		
-		InputStream inputStream =ClassLoader.getSystemResourceAsStream("hadoop/hadoop.properties");
-		properties.load(inputStream);
-		String	uri = properties.getProperty("uri")+":"+properties.getProperty("port");
-		Configuration conf = new Configuration();
-		fileSystem = FileSystem.get(URI.create(uri),conf);
-	}	
-	
-	@Test
-	public void testUpload() throws Exception {
-		fileSystem.copyFromLocalFile(new Path("E:\\测试数据.zip"), new Path("/测试数据.zip"));
-		fileSystem.close();
-	}
-	
-	@Test
-	public void testGet() throws Exception {
-		fileSystem.copyToLocalFile(new Path("/测试数据.zip"), new Path("F:\\测试数据.zip"));
-	}
-	
-	@Test
-	public void testDelete() throws Exception {
-		fileSystem.delete(new Path("/测试数据.zip"));
-	}
-}
+//package com.wqj.hdfs;
+//
+//import java.io.InputStream;
+//import java.net.URI;
+//import java.util.Properties;
+//
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.fs.FileSystem;
+//import org.apache.hadoop.fs.Path;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//
+//
+//
+///**
+// * @Description: TODO
+// * @author wqj
+// * @date 2018年3月18日 下午8:36:53
+// */
+//public class HdfsClientDemo {
+//
+//
+//	FileSystem fileSystem =null;
+//	@Before
+//	public void init() throws Exception {
+//		Properties properties= new Properties();
+//
+//		InputStream inputStream =ClassLoader.getSystemResourceAsStream("hadoop/hadoop.properties");
+//		properties.load(inputStream);
+//		String	uri = properties.getProperty("uri")+":"+properties.getProperty("port");
+//		Configuration conf = new Configuration();
+//		fileSystem = FileSystem.get(URI.create(uri),conf);
+//	}
+//
+//	@Test
+//	public void testUpload() throws Exception {
+//		fileSystem.copyFromLocalFile(new Path("E:\\测试数据.zip"), new Path("/测试数据.zip"));
+//		fileSystem.close();
+//	}
+//
+//	@Test
+//	public void testGet() throws Exception {
+//		fileSystem.copyToLocalFile(new Path("/测试数据.zip"), new Path("F:\\测试数据.zip"));
+//	}
+//
+//	@Test
+//	public void testDelete() throws Exception {
+//		fileSystem.delete(new Path("/测试数据.zip"));
+//	}
+//}
