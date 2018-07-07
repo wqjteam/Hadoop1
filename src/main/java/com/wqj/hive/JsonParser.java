f240003914c45e8965192bc11e3bcc842f9b86b3
githubwqj <893453172@qq.com>
2018-07-07 18:37:11 +0800

    1

---
+++ src/main/java/com/wqj/hive/JsonParser.java
@@ -0,0 +1,15 @@
+package com.wqj.hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * @Auther: wqj
+ * @Date: 2018/7/7 18:35
+ * @Description:
+ */
+public class JsonParser extends UDF {
+
+    public String evaluate(String jsonLine){
+        return null;
+    }
+}
