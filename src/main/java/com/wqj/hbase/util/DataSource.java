60d9b0d1f9438d14a3dc3362d068ff9086df4a82
githubwqj <893453172@qq.com>
2018-07-11 14:37:31 +0800

    1

---
+++ src/main/java/com/wqj/hbase/util/DataSource.java
@@ -0,0 +1,34 @@
+package com.wqj.hbase.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * @Auther: wqj
+ * @Date: 2018/7/10 18:07
+ * @Description: 使用phoenix操作hbase
+ */
+public class DataSource {
+    public Connection GetConnection(){
+        Connection cc = null;
+        String driver = "org.apache.phoenix.jdbc.PhoenixDriver";
+        String url = "jdbc:phoenix:hadoop1:2181";
+
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (cc == null) {
+            try {
+                cc = DriverManager.getConnection(url);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return cc;
+    }
+}
