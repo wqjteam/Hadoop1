70a0f201d643b7f35b7216439dcc585c3a37cc3b
githubwqj <893453172@qq.com>
2018-07-20 18:13:56 +0800

    1

--- src/main/java/com/wqj/App.java
+++ src/main/java/com/wqj/App.java
@@ -1,5 +1,10 @@
 package com.wqj;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,27 @@ public class App
 {
     public static void main( String[] args )
     {
+        HashMap map=new HashMap<String,String>();
+        map.put(null,null);
+        Hashtable table=new Hashtable<String,String>();
+        //报错
+        table.put("1",null);
+
+        List list=null;
+        Set set=null;
+        System.out.println("11111111111");
+        table.put(null,null);
+
+
+
+
+
+
+
+
+
+
+
         System.out.println( "Hello World!" );
     }
 }
